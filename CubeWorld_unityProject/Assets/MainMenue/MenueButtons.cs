8a65c792a2033c9871127fe5003da10b0e74868a
net-attack <philwagner@me.com>
2017-12-03 21:57:22 +0100

    Level Select Menue and first Scene

---
+++ CubeWorld_unityProject/Assets/MainMenue/MenueButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MenueButtons : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject loadingImage;
+
+
+    public void LoadScene()
+    {
+        loadingImage.SetActive(true);
+        Application.LoadLevel(1);
+    }
+}
+
+
